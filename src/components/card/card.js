35db3f8980e5d47cdde33ef1af7217cbdd7f1b0e
Diego dos Santos <diego@DESKTOP-0IRUF00.localdomain>
2022-08-28 00:45:53 -0300

    solve

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,10 +1,10 @@
 import style from "./card.style.module.css";
-import we from '/home/diego/documents/birthday/happybirthday/src/assets/she-and-me.jpeg'
+import image from '/home/diego/documents/birthday/happybirthday/src/assets/she-and-me.jpeg'
 
 export default function Card() {
   return (
     <div className={style.container}>
-      <img className={style.we} src={we}/>
+      <img className={style.we} src={image}/>
     </div>
   );
 }
