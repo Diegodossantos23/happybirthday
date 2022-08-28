3212512b1b5e38e46890376593eebc58ed82d727
Diego dos Santos <diego@DESKTOP-0IRUF00.localdomain>
2022-08-27 23:34:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Card from "./components/card/card";
+import style from "./app.style.module.css";
+
+export default function App() {
+  return (
+    <div className={style.container}>
+      <Card/>
+    </div>
+  );
+}
---
+++ src/components/card/card.js
@@ -0,0 +1,10 @@
+import style from "./card.style.module.css";
+
+export default function Card() {
+  return (
+    <div className={style.container}>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
